96aa1698eef0896f02f32f8e5c2193f4e3ae53d0
dinhthanh143 <neonlimea@gmail.com>
2024-11-20 14:54:10 +0700

    Add files via upload

---
+++ b2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+   int first_number = 20;
+   
+   int second_number = 30;
+   
+   int total = first_number + second_number;
+   
+   
+   printf("tong la %d\n",total);
+   
+   
+   
+   
+   return 0;
+}
+
