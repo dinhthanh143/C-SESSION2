c85b949ab8a7a2a1db55b49fb24fdace147ddcb2
dinhthanh143 <neonlimea@gmail.com>
2024-11-20 14:52:18 +0700

    Add files via upload

---
+++ b4.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+    int canh = 3;
+    printf("canh la %d \n",canh);
+    
+    int chu_vi = 4 * canh;
+    
+    printf("chu vi la %d\n",chu_vi);
+    
+    int dien_tich = canh * canh;
+    printf("dien tich la %d",dien_tich);
+   return 0;
+}
+
