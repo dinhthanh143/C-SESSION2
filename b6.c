1ba0ec402d4b64b4c5dfa2e96de02b71fccbb022
dinhthanh143 <neonlimea@gmail.com>
2024-11-21 10:41:26 +0700

    Add files via upload

--- b6.c
+++ b6.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 
 int main(){
-  const float pi = 3.14;
+  const float PI = 3.14;
   
   int ban_kinh = 4;
    printf("ban kinh hinh tron la %d\n", ban_kinh);
   
-  float chu_vi = 2*pi*ban_kinh;
+  float chu_vi = 2*PI*ban_kinh;
   
   printf("chu vi hinh tron la %.2f\n", chu_vi);
   
-  float dien_tich = pi*(ban_kinh*ban_kinh);
+  float dien_tich = PI*(ban_kinh*ban_kinh);
   
   printf("dien tich hinh tron la %.2f", dien_tich);
   
